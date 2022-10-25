5b278fbe1027e6f27ea51176387b520bb99efbdb
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2022-10-25 21:50:50 +0600

    all routes created

--- index.js
+++ index.js
@@ -8,6 +8,29 @@ app.get('/courses-categries', (req, res) => {
   res.send(categories);
 });
 
+app.get('/all-courses', (req, res) => {
+  res.send(courses);
+});
+
+app.get('/all-courses/:id', (req, res) => {
+  const singleCourseById = courses.filter(
+    (course) => course.id == req.params.id
+  );
+  res.send(singleCourseById);
+});
+
+app.get('/courses/:id', (req, res) => {
+  const coursesById = courses.filter(
+    (course) => course.category_id == req.params.id
+  );
+  res.send(coursesById);
+});
+
+app.get('/popular-courses', (req, res) => {
+  const coursesByPopularity = courses.filter((course) => course.isPopular);
+  res.send(coursesByPopularity);
+});
+
 app.listen(5000, () => {
   console.log('Listening on port 5000');
 });
