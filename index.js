e49fb2c0e6f36880338248e29688005887e10f63
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2022-10-25 21:49:38 +0600

    categories route created

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const courses = require('./data/courses');
 const categories = require('./data/categories.json');
 
 const app = express();
+
+app.get('/courses-categries', (req, res) => {
+  res.send(categories);
+});
+
+app.listen(5000, () => {
+  console.log('Listening on port 5000');
+});
