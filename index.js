f7d1d516da659da655fcb7a2096405e9e5f3a468
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2023-01-14 20:40:34 +0600

    adding comments

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
+//Importing packages
 const express = require('express');
 const courses = require('./data/courses');
 const categories = require('./data/categories.json');
 const cors = require('cors');
 
+//Express app
 const app = express();
 
+//Enabling Cors
 app.use(cors());
 
-app.get('/courses-categories', (req, res) => {
+//Getting all the categories of courses
+app.get('/courses-categories', (_req, res) => {
   res.send(categories);
 });
 
-app.get('/all-courses', (req, res) => {
+//Getting all the courses
+app.get('/all-courses', (_req, res) => {
   res.send(courses);
 });
 
+//Getting a course by the course id
 app.get('/all-courses/:id', (req, res) => {
+  //Finding the particular course
   const singleCourseById = courses.filter(
     (course) => course.id == req.params.id
   );
   res.send(singleCourseById);
 });
 
+//Getting a course by the category id
 app.get('/courses/:id', (req, res) => {
+  //Finding the particular category
   const coursesById = courses.filter(
     (course) => course.category_id == req.params.id
   );
   res.send(coursesById);
 });
 
-app.get('/popular-courses', (req, res) => {
+//Getting all the courses that are popular
+app.get('/popular-courses', (_req, res) => {
   const coursesByPopularity = courses.filter((course) => course.isPopular);
   res.send(coursesByPopularity);
 });
